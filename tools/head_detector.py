12955a13b8e803a2688a89e6618259d360275e2c
chenkai <ckmessi@163.com>
2018-01-12 19:13:31 +0800

    arange head detector

--- tools/head_detector.py
+++ tools/head_detector.py
@@ -24,78 +24,13 @@ import scipy.io as sio
 import caffe, os, sys, cv2
 import argparse
 
-CLASSES = ('__background__',
-           'head')
+CLASSES = ('__background__', 'head')
 
 NETS = {'vgg16': ('VGG16',
                   'VGG16_faster_rcnn_final.caffemodel'),
         'zf': ('ZF',
                   'ZF_faster_rcnn_final.caffemodel')}
 
-
-def vis_detections(im, class_name, dets, thresh=0.5):
-    """Draw detected bounding boxes."""
-    inds = np.where(dets[:, -1] >= thresh)[0]
-    if len(inds) == 0:
-        return
-    
-    print dets[inds,:], class_name
-
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
-    for i in inds:
-        bbox = dets[i, :4]
-        score = dets[i, -1]
-
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
-
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
-
-def demo(net, image_name):
-    """Detect object classes in an image using pre-computed object proposals."""
-
-    # Load the demo image
-    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
-    im_file = os.path.join('/home/tangwang/test3/', image_name)
-    # im_file = os.path.join(cfg.DATA_DIR, 'HEADdevkit2018/HEAD2018/JPEGImages/', image_name)
-    im = cv2.imread(im_file)
-
-    # Detect all object classes and regress object bounds
-    timer = Timer()
-    timer.tic()
-    scores, boxes = im_detect(net, im)
-    timer.toc()
-    print ('Detection took {:.3f}s for '
-           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
-
-    # Visualize detections for each class
-    CONF_THRESH = 0.8
-    NMS_THRESH = 0.3
-    for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1 # because we skipped background
-        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
-        cls_scores = scores[:, cls_ind]
-        dets = np.hstack((cls_boxes,
-                          cls_scores[:, np.newaxis])).astype(np.float32)
-        keep = nms(dets, NMS_THRESH)
-        dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
 
 def parse_args():
     """Parse input arguments."""
@@ -107,47 +42,60 @@ def parse_args():
                         action='store_true')
     parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16]',
                         choices=NETS.keys(), default='vgg16')
-
     args = parser.parse_args()
-
     return args
 
-if __name__ == '__main__':
+class HeadDetector(object):
+    def __init__(self):
+        self.initial_network()
 
-    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+    def detect_img(self, img):
+        scores, boxes = im_detect(self.net, img)
+        CONF_THRESH = 0.8
+        NMS_THRESH = 0.3
+        detect_list = []
+        for cls_ind, cls in enumerate(CLASSES[1:]):
+            cls_ind += 1 # because we skipped background
+            cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+            cls_scores = scores[:, cls_ind]
+            dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32)
+            keep = nms(dets, NMS_THRESH)
+            dets = dets[keep, :]
+            inds = np.where(dets[:,-1] >= CONF_THRESH)[0]
+            dets = dets[inds, :]
+            detect_list.extend(dets)
+        return detect_list
 
-    args = parse_args()
-
-    prototxt = os.path.join(cfg.ROOT_DIR, 'models', 'head', 'VGG16','faster_rcnn_end2end','test.prototxt')
-
-    iter_num = 50000
-    caffemodel = os.path.join(cfg.DATA_DIR, 'vgg16_faster_rcnn_iter_' + str(iter_num) + '.caffemodel')
-
-
-    if not os.path.isfile(caffemodel):
-        raise IOError(('{:s} not found.\nDid you run ./data/script/'
+    def initial_network(self):
+        cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+        args = parse_args() 
+        prototxt = os.path.join(cfg.ROOT_DIR, 'models', 'head', 'VGG16','faster_rcnn_end2end','test.prototxt')
+        iter_num = 50000
+        caffemodel = os.path.join(cfg.DATA_DIR, 'vgg16_faster_rcnn_iter_' + str(iter_num) + '.caffemodel')
+        if not os.path.isfile(caffemodel):
+            raise IOError(('{:s} not found.\nDid you run ./data/script/'
                        'fetch_faster_rcnn_models.sh?').format(caffemodel))
+        if args.cpu_mode:
+            caffe.set_mode_cpu()
+        else:
+            caffe.set_mode_gpu()
+            caffe.set_device(args.gpu_id)
+            cfg.GPU_ID = args.gpu_id
+    
+        self.net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+        print '\n\nLoaded network {:s}'.format(caffemodel)
+        # Warmup on a dummy image
+        im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+        for i in xrange(2):
+            _, _= im_detect(self.net, im)
 
-    if args.cpu_mode:
-        caffe.set_mode_cpu()
-    else:
-        caffe.set_mode_gpu()
-        caffe.set_device(args.gpu_id)
-        cfg.GPU_ID = args.gpu_id
-    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+if __name__ == '__main__':
+    
+    head_detector = HeadDetector()   
+    img_path = os.path.join('/home/tangwang/test/test1/', '0000001.jpg')
+    img = cv2.imread(img_path)
+    detect_list = head_detector.detect_img(img)
+    for detect_item in detect_list:
+        print(detect_item)
 
-    print '\n\nLoaded network {:s}'.format(caffemodel)
-
-    # Warmup on a dummy image
-    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
-    for i in xrange(2):
-        _, _= im_detect(net, im)
-
-    im_names = os.listdir('/home/tangwang/test3')
-    im_names = im_names[0:10]
-    for im_name in im_names:
-        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
-
-    plt.show()
+    
